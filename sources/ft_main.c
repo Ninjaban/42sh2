5b100d9d3f8f72674aba73a0e08028ef17428983
Nathan <jcarra@student.42.fr>
2018-02-07 16:57:48 +0100

    main test

--- sources/ft_main.c
+++ sources/ft_main.c
@@ -10,24 +10,18 @@ int		main()
 {
 	ft_print_initialize();
 	box(g_internal_context.term.base.window, 0, 0);
-//	wmove(g_internal_context.term.base.window, 0, 0);
-//	wprintw(g_internal_context.term.base.window, "TERM");
 	wrefresh(g_internal_context.term.base.window);
 
 	box(g_internal_context.term.zone[ZONE_HISTORY].window, 0, 0);
-//	wprintw(g_internal_context.term.zone[ZONE_HISTORY].window, "HISTORY");
 	touchwin(g_internal_context.term.base.window);
 	wrefresh(g_internal_context.term.zone[ZONE_HISTORY].window);
 	box(g_internal_context.term.zone[ZONE_PRINT].window, 0, 0);
-//	wprintw(g_internal_context.term.zone[ZONE_PRINT].window, "PRINT");
 	touchwin(g_internal_context.term.base.window);
 	wrefresh(g_internal_context.term.zone[ZONE_PRINT].window);
 	box(g_internal_context.term.zone[ZONE_TREE].window, 0, 0);
-//	wprintw(g_internal_context.term.zone[ZONE_TREE].window, "TREE");
 	touchwin(g_internal_context.term.base.window);
 	wrefresh(g_internal_context.term.zone[ZONE_TREE].window);
 	box(g_internal_context.term.zone[ZONE_CMD].window, 0, 0);
-//	wprintw(g_internal_context.term.zone[ZONE_CMD].window, "CMD");
 	touchwin(g_internal_context.term.base.window);
 	wmove(g_internal_context.term.zone[ZONE_CMD].window, 1, 1);
 	curs_set(1);
